83c8de95b728c9ffb8ea826204e139d84e215fb1
DM Kishi <dm.kishi@gmail.com>
2016-05-04 11:36:53 -0700

    Improve Gulp build system

--- dist/hasJS.js
+++ dist/hasJS.js
@@ -1,6 +1,8 @@
-// hasJS v1.0.4 (2016-05-04T16:00:47.290Z)
-// Most basic JavaScript detector. Replaces '.no-js' with '.js' in the HTML tag, and that's it.
-// <https://github.com/dmkishi/hasJS>
+/* hasJS v1.0.9 (2016-05-04T18:35:04.547Z)
+ * Most basic JavaScript detector. Replaces '.no-js' with '.js' in the HTML tag, and that's it.
+ * <https://github.com/dmkishi/hasJS>
+ */
+
 (function() {
   var html = document.getElementsByTagName('html')[0];
   html.className = html.className.replace(/\bno-js\b/, 'js');
