a055485e075e5d760f6f5c70c88a9b85d19e39fd
DM Kishi <dm.kishi@gmail.com>
2016-05-04 09:02:25 -0700

    Add homepage to build files

--- dist/hasJS.js
+++ dist/hasJS.js
@@ -1,6 +1,6 @@
-// hasJS v1.0.4 (2016-05-02T20:07:42.017Z)
+// hasJS v1.0.4 (2016-05-04T16:00:47.290Z)
 // Most basic JavaScript detector. Replaces '.no-js' with '.js' in the HTML tag, and that's it.
-
+// <https://github.com/dmkishi/hasJS>
 (function() {
   var html = document.getElementsByTagName('html')[0];
   html.className = html.className.replace(/\bno-js\b/, 'js');
